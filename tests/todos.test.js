476257fc23d2c71fe9dddee27f7ee9f654435ea7
dom-m17 <dominicmaynard00@gmail.com>
2024-08-08 11:59:51 +0100

    ticket 5 and 6

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -2,6 +2,7 @@ import {test, expect} from '@playwright/test';
 
 test("practice test should pass", async ({page}) => {
     await page.goto("http://localhost:5432")
-}
-
-)
+    // get the text box
+    await page.getByRole('textbox', { name: 'New Todo' }).click()
+    await page.getByRole('textbox', { name: 'New Todo' }).fill("Wash the other car")
+})
